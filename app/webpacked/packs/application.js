95853334e6ea2d495fd032183ce8726e3b58c843
Oever González <software@notengobattery.com>
2021-03-12 23:52:11 -0600

    Configure Webpacker Assets Pipeline

--- app/webpacked/packs/application.js
+++ app/webpacked/packs/application.js
@@ -1,15 +1,12 @@
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
+// a relevant structure within packs/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-// Import Normalize.css to have a consistent starting point
-import "@csstools/normalize.css/normalize"
 
 Rails.start()
 Turbolinks.start()
