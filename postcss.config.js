b02db94718b07265bdb1b79a6e94fd51801519e3
Oever González <software@notengobattery.com>
2021-04-24 20:12:13 -0600

    Use TypeScript by default

--- postcss.config.js
+++ postcss.config.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
 // Add plugins at the end of the chain
 postcss.plugins.push(
   [
-    // require('autoprefixer')
+    require('autoprefixer')
   ]
 )
 
