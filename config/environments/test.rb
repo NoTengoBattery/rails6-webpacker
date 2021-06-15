485106abbe6eb703b329f16ed2c0e429b83c018c
Oever González <software@notengobattery.com>
2021-06-15 16:02:47 -0600

    [FIX] Small typo fixes
    Signed-off-by: Oever González <software@notengobattery.com>

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -63,7 +63,4 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Do not try to send mails when testing
-  config.action_mailer.delivery_method = :test
 end
--- spec/features/placeholders_spec.rb
+++ spec/features/placeholders_spec.rb
@@ -3,7 +3,7 @@ require "shared_rutines"
 
 RSpec.describe "placeholder page", type: :system do
   describe "placeholders" do
-    it "shows the 'Uner construction' page" do
+    it "shows the 'Under construction' page" do
       visit root_path
       expect(page).to have_content(I18n.t(:under_construction))
     end
